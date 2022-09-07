b401ef778c190a0c6473a7493721d9aecefa3977
pedro <pedrosouza185@gmail.com>
2022-09-07 14:39:44 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors')
+const rateLimit = require('express-rate-limit')
+require('dotenv').config()
+
+const PORT = process.env.POR || 3000
+
+const app = express()
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 100
+})
+app.use(limiter)
+app.set('trust proxy', 1)
+
+// Enable cors
+app.use(cors())
+
+// Routes
+app.use('/api', require('./routes/index'))
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
---
+++ routes/index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const dns = require('node:dns')
+const router = express.Router()
+const needle = require('needle')
+const apicache = require('apicache')
+
+// Enviroment variables
+const API_BASE_URL = process.env.API_BASE_URL
+const API_KEY_NAME = process.env.API_KEY_NAME
+const API_KEY_VALUE = process.env.API_KEY_VALUE
+
+// Init cache
+let cache = apicache.middleware
+
+router.get('/', cache('1 day'), async (req, res) => {
+  async function connectToApi() {
+    const params = new URLSearchParams({
+      [API_KEY_NAME]: API_KEY_VALUE,
+      ...req.query
+    })
+
+    const apiRes = await needle('get', `${API_BASE_URL}?${params}`)
+    const data = apiRes.body
+
+    // Log the request to the public API
+    if (process.env.NODE_ENV !== 'production') {
+      console.log(`REQUEST: ${API_BASE_URL}?${params}`)
+    }
+
+    res.status(200).json(data)
+  }
+
+  try {
+    // Resolve a domain if needed
+    if ('domain' in req.query) {
+      const domain = req.query.domain
+      try {
+        let obj = await dns.promises.resolveAny(domain)
+        ipAddress = obj[0].address
+        req.query.ip = ipAddress
+        connectToApi()
+      } catch (error) {
+        console.log(error)
+        res.send(error)
+      }
+    } else {
+      connectToApi()
+    }
+  } catch (error) {
+    res.status(500).json({ error })
+  }
+})
+
+module.exports = router
